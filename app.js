5bb82a1bad57f185428e6e1bdf2467011243cfa5
mengzhiai <458677565@qq.com>
2020-12-24 23:54:33 +0800

    添加mysql连接池

--- app.js
+++ app.js
@@ -2,20 +2,33 @@
  * @Date: 2020-12-24 22:41:34
  * @Description: app.js
  * @LastEditors: jun
- * @LastEditTime: 2020-12-24 23:16:34
+ * @LastEditTime: 2020-12-24 23:43:02
  * @FilePath: \mall-server\app.js
  */
 const Koa = require('koa');
 const app = new Koa();
 
 
+const koaBody = require('koa-body');
+
+const path = require('path');
+
+const koaStatic = require('koa-static');
+
 
 // 使用路由中间件
 const routers = require('./app/router');
-
-
 app.use(routers.routes()).use(routers.allowedMethods());
 
+
+// 配置静态资源加载中间件
+app.use(koaStatic(
+  path.join(__dirname, './public')
+))
+
+
+// 表单中间件
+app.use(koaBody());
 
 app.listen(3000, () => {
   console.log('server');
