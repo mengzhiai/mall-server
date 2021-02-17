b89a87242c5f3834be8741e2f6f8c04b92f45095
mengzhiai <458677565@qq.com>
2021-02-18 01:12:12 +0800

    修改

--- app/middleware/validator.js
+++ app/middleware/validator.js
@@ -2,7 +2,7 @@
  * @Date: 2021-01-27 22:30:43
  * @Description: 验证参数
  * @LastEditors: jun
- * @LastEditTime: 2021-02-18 01:04:11
+ * @LastEditTime: 2021-02-18 01:12:08
  * @FilePath: \mall-server\app\middleware\validator.js
  */
 
@@ -35,13 +35,13 @@ module.exports = {
     }
 
     // 商品分类
-    if(!params.category || validator.isEmpty(params.category)) {
+    if(!params.category) {
       let error = new ParameterException('商品分类不能为空');
       throw error;
     }
 
     // 商品价格
-    if(!params.productPrice || validator.isEmpty(params.productPrice)) {
+    if(!params.productPrice) {
       let error = new ParameterException('商品价格不能为空');
       throw error;
     }
