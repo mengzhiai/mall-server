7fe01d686f5c9c061db097fd8db939b5d2fa13b1
mengzhiai <458677565@qq.com>
2021-03-20 15:10:07 +0800

    修改文件上传

--- app/controller/commonController.js
+++ app/controller/commonController.js
@@ -2,7 +2,7 @@
  * @Date: 2021-02-20 23:21:07
  * @Description: 公用
  * @LastEditors: jun
- * @LastEditTime: 2021-02-20 23:43:17
+ * @LastEditTime: 2021-03-20 15:07:33
  * @FilePath: \mall-server\app\controller\commonController.js
  */
 
@@ -23,13 +23,13 @@ module.exports = {
   async upload(ctx) {
     // 上传单个文件
     const file = ctx.request.files.file // 获取上传文件
-    // 创建可读流
+    /* // 创建可读流
     const reader = fs.createReadStream(file.path);
     let filePath = path.join(__dirname) + `/${file.name}`;
     // 创建可写流
     const upStream = fs.createWriteStream(filePath);
     // 可读流通过管道写入可写流
-    reader.pipe(upStream);
+    reader.pipe(upStream); */
     return ctx.body = {
       msg: '上传成功',
       code: 200,
